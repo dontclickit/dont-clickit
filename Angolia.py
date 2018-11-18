e13402ce68f22285ce32e2a3447c3828e3a7f806
Faisal <faisalamin1999@googlemail.com>
2018-11-18 09:00:13 +0000

    Updates to Angolia database manipulation

--- Angolia.py
+++ Angolia.py
@@ -2,8 +2,8 @@ from algoliasearch import algoliasearch
 import mysql
 import mysql.connector
 
-
-def main(): 
+currentbatch=0
+def main(currentbatch): 
     client = algoliasearch.Client("CBJLT1KBQH","2ab19a08e4be64e5a5f55dd080c80f5d")
     indexName = "database2"
     #client.delete_index(indexName) #do not delete the index anymore
@@ -20,28 +20,35 @@ def main():
     cursor.execute("SELECT * FROM reports")
     actors = list(cursor.fetchall())
 
-    print(actors[2]["reports"])
         
     for i in range(len(actors)):
-        currLink = actors[i]["link"]
+        currLink = actors[i]["link"][:500]
         if(index.search(currLink)["hits"]):
             newReport = index.search(currLink)["hits"][0]["reports"] + actors[i]["reports"]
-            print(newReport)
             objectID = index.search(currLink)["hits"][0]["objectID"]
             bigBatch = index.search(currLink)["hits"][0]["biggestbatch"]
             bigBatchSize = index.search(currLink)["hits"][0]["biggestbatchsize"]
             index.partial_update_object({"objectID": objectID, "reports":newReport})
-            if(bigBatch < actors[i]["biggestbatch"]):
-                index.partial_update_object({"objectID": objectID, "biggestbatch":actors[i]["biggestbatch"]})
-            if(bigBatchSize < actors[i]["biggestbatchsize"]):
-                index.partial_update_object({"objectID": objectID, "biggestbatchsize":actors[i]["biggestbatchsize"]})
+##            if(bigBatch < actors[i]["biggestbatch"]):
+##                index.partial_update_object({"objectID": objectID, "biggestbatch":actors[i]["biggestbatch"]})
+            if(bigBatchSize < actors[i]["reports"]):#"biggestbatchsize"]):
+                index.partial_update_object({"objectID": objectID, "biggestbatchsize":actors[i]["reports"]})
+                index.partial_update_object({"objectID": objectID, "biggestbatch":currentbatch})
+                
+               
+               
         else:
             index.add_objects([actors[i]])
 
+
+    cursor.execute("TRUNCATE TABLE reports")
+    cnx.commit()
+    
     cursor.close()
     cnx.close()
+    return currentbatch+1
 
-main()
+currentbatch=main(currentbatch)
     
         
             
