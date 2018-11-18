3249386a00ecbfaa9c6a5eb2775a3228c45cd1ca
Faisal <faisalamin1999@googlemail.com>
2018-11-18 06:42:25 +0000

    Updated Python file for adding data to Angolia database

--- Angolia.py
+++ Angolia.py
@@ -5,8 +5,8 @@ import mysql.connector
 
 def main(): 
     client = algoliasearch.Client("CBJLT1KBQH","2ab19a08e4be64e5a5f55dd080c80f5d")
-    indexName = "database"
-    client.delete_index(indexName)
+    indexName = "database2"
+    #client.delete_index(indexName) #do not delete the index anymore
     index = client.init_index(indexName)
 
     #actors = [{"link":"www.lukeisanidiot.com", "reports":45},{"link":"www.bao=shiv.com","reports":45}]
@@ -20,10 +20,32 @@ def main():
     cursor.execute("SELECT * FROM reports")
     actors = list(cursor.fetchall())
 
-    index.add_objects(actors)
+    for i in range(len(actors)):
+        currLink = actors[i]["link"]
+        if(index.search(currLink)["hits"]):
+            newReport = index.search(currLink)["hits"][0]["reports"] + actors[i]["reports"]
+            print(newReport)
+            objectID = index.search(currLink)["hits"][0]["objectID"]
+            bigBatch = index.search(currLink)["hits"][0]["biggestbatch"]
+            bigBatchSize = index.search(currLink)["hits"][0]["biggestbatchsize"]
+            index.partial_update_object({"objectID": objectID, "reports":newReport})
+            if(bigBatch < actors[i]["biggestbatch"]):
+                index.partial_update_object({"objectID": objectID, "biggestbatch":actors[i]["biggestbatch"]})
+            if(bigBatchSize < actors[i]["biggestbatchsize"]):
+                index.partial_update_object({"objectID": objectID, "biggestbatchsize":actors[i]["biggestbatchsize"]})
+        else:
+            index.add_objects([actors[i]])
 
     cursor.close()
     cnx.close()
+
+main()
+    
+        
+            
+        
+        
+
     
 
     
