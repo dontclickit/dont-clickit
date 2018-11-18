699cda0c612b04641ded2adffb1869f6aaf59a37
Faisal <faisalamin1999@googlemail.com>
2018-11-18 00:50:41 +0000

    Server now recognizes when link is clickbait

--- server.js
+++ server.js
@@ -7,9 +7,37 @@ var index = client.initIndex("database");
 	searchableAttributes: ["link"]
 }); */ //already run - makes only searchable by "link" attribute
 
-index.search("3").then(result => {
-	console.log(result.nbHits); //this shows number of records found
-	for(var hit of result.hits) {
-		console.log(hit);
-	}
-});
+var linksReceived = ["https://twitter.com"]; //will hold all the links sent by user
+
+var linksArray = []; //will hold all links found
+var reportsArray = []; //will hold all number of reports for each link
+var clickBait = []; //0 for not clickbait, 1 for clickbait
+
+for(var x of linksReceived){
+	index.search(x).then(result => {
+		//console.log(result.nbHits); //this shows number of records found
+		for(var hit of result.hits) {
+			console.log(hit.link);
+			linksArray.push(hit.link);
+			reportsArray.push(hit.reports);
+			clickBait.push(0)
+		}
+		console.log(linksArray);
+		console.log(reportsArray);
+		console.log(clickBait);
+
+		for(var i = 0; i < linksArray.length; i++) {
+			if(reportsArray[i] > 100){ //this is how we determin clickbait articles at the moment
+				clickBait[i] = 1;
+			}
+		}
+
+		console.log(linksArray);
+		console.log(reportsArray);
+		console.log(clickBait);
+
+
+
+	})};
+
+
